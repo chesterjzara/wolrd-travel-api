687f4ccd6e76a72e67e8082c5e14c408142fd5e6
Chester Zara <chesterjzara@gmail.com>
2019-06-08 10:58:26 -0500

    Added cors-rack gem and configured to allow React to access'

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'localhost:3006', 'shielded-taiga-28162.herokuapp.com/', 'https://shielded-taiga-28162.herokuapp.com/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
