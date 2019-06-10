6cf679fe49671cce50e16df7e5f0b35f3e12075b
Chester Zara <chesterjzara@gmail.com>
2019-06-10 15:28:01 -0500

    opening cors to all

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://shielded-taiga-28162.herokuapp.com/'
+    # origins 'https://shielded-taiga-28162.herokuapp.com/'
+    origins '*'
     
     resource '*',
       headers: :any,
